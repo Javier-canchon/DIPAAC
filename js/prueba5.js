cd196a04d3798660d94cad325ac1710a017060d3
javier.canchon <javier.canchon@hotmail.com>
2019-10-05 13:51:25 -0500

    version inicial

---
+++ js/prueba5.js
@@ -0,0 +1,12 @@
+ 
+
+src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js";
+type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js";
+
+$("#Juan1").draggable();
+$("#Juan2").draggable();
+$("#Juan3").draggable();
+$("#Juan4").draggable();
+
+
+alert("hola");
