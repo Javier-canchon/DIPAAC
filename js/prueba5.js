afac52a1b521e4351578763b54ff1975d60bab76
javier.canchon <javier.canchon@hotmail.com>
2020-01-27 14:04:05 -0500

    se terminan algunas pruebas y se empieza a trabajar en el menu de cada una

--- js/prueba5.js
+++ js/prueba5.js
@@ -1,14 +1,13 @@
  
 var aciertos = 0;
 var errores = 0;
-var omisiones = 10; 
+var omisiones = 5; 
 
-// primera pregunta
+// pregunta sobre casa/ caza
 $("#Casa").click(function(event) {
 	$('#Casa').css('color', 'black');
 aciertos++;
 omisiones--;
-alert(aciertos);
 $('#Casa').unbind("click");
 $('#Caza').unbind("click");
 });
@@ -16,7 +15,93 @@ $('#Caza').unbind("click");
 $("#Caza").click(function(event) {
 	$('#Caza').css('color', 'black');
 errores++;
-alert(errores);
 $('#Casa').unbind("click");
 $('#Caza').unbind("click");
+});
+
+// pregunta sobre casa/ caza
+$("#Hablando").click(function(event) {
+	$('#Hablando').css('color', 'black');
+aciertos++;
+omisiones--;
+$('#Hablando').unbind("click");
+$('#Ablando').unbind("click");
+});
+
+$("#Ablando").click(function(event) {
+	$('#Ablando').css('color', 'black');
+errores++;
+$('#Hablando').unbind("click");
+$('#Ablando').unbind("click");
+});
+
+
+// pregunta sobre vaso/ Bazo
+$("#Vaso").click(function(event) {
+	$('#Vaso').css('color', 'black');
+aciertos++;
+omisiones--;
+$('#Vaso').unbind("click");
+$('#Bazo').unbind("click");
+});
+
+$("#Bazo").click(function(event) {
+	$('#Bazo').css('color', 'black');
+errores++;
+$('#Vaso').unbind("click");
+$('#Bazo').unbind("click");
+});
+
+
+// pregunta sobre hola/ ola
+$("#Ola").click(function(event) {
+	$('#Ola').css('color', 'black');
+aciertos++;
+omisiones--;
+$('#Ola').unbind("click");
+$('#Hola').unbind("click");
+});
+
+$("#Hola").click(function(event) {
+	$('#Hola').css('color', 'black');
+errores++;
+$('#Ola').unbind("click");
+$('#Hola').unbind("click");
+});
+
+
+// pregunta sobre vello/ bello
+$("#Vello").click(function(event) {
+	$('#Vello').css('color', 'black');
+aciertos++;
+omisiones--;
+$('#Vello').unbind("click");
+$('#Bello').unbind("click");
+});
+
+$("#Bello").click(function(event) {
+	$('#Bello').css('color', 'black');
+errores++;
+$('#Vello').unbind("click");
+$('#Bello').unbind("click");
+});
+
+
+
+// boton-----------------------------------------
+$("#enviar").click(function(event) {
+    $('#resultado').html("El total de puntos fue: " + aciertos);
+    $('#omision').html("El puntaje de omisiones fue: " + omisiones);
+    $('#Errores').html("El puntaje de errores fue: " + errores);
+
+    //var segundo = JSON.parse(sessionStorage.getItem("segundo"));
+    //var minuto = JSON.parse(sessionStorage.getItem("minuto"));
+
+    //$('#resultado').append("  El estudiante se demoró: " + segundo + " segundos " + "y " + minuto + " minutos");
+
+ var segundo = JSON.parse(sessionStorage.getItem("segundo"));
+    var minuto = JSON.parse(sessionStorage.getItem("minuto"));
+
+    $('#resultado').append(" El estudiante se demoró: " + segundo + " segundos " + "y " + minuto + " minutos");
+
 });
