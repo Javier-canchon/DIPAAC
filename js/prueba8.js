c87c179d7af64c8851a6c1e5d8c27d91acdee17f
javier.canchon <javier.canchon@hotmail.com>
2020-01-28 15:37:18 -0500

    prueba 8 sobre lexico semantico, se trabaja sobre el bosquejo de otra prueba y se añade el codigo principal de java script de la misma

---
+++ js/prueba8.js
@@ -0,0 +1,10 @@
+$('#guardar').click(function() {
+
+   if ($('#tren').val() === 'transporte'||$('#tren').val() === 'Transporte') 
+     {
+        alert('suma un punto');
+    } else  {
+       alert('suma un error');
+   }
+});
+
