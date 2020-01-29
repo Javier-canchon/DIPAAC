4d19cd3bc839ef103e7e34c4362d800b56e2a316
javier.canchon <javier.canchon@hotmail.com>
2020-01-29 17:36:35 -0500

    se agrego mas codigo para la prueba8 de lexico semantico

--- js/prueba8.js
+++ js/prueba8.js
@@ -1,10 +1,39 @@
-$('#guardar').click(function() {
 
-   if ($('#tren').val() === 'transporte'||$('#tren').val() === 'Transporte') 
+
+var aciertos = 0;
+var errores = 0;
+
+$('#guardar1').click(function() {
+
+   if ($('#transporte').val() === 'transporte'||$('#transporte').val() === 'Transporte'||$('#transporte').val() === 'TRANSPORTE') 
      {
-        alert('suma un punto');
+     	aciertos++
+     	$('#guardar1').unbind("click");
+     	$('#guardar1').css('color', 'black');
+     	alert(aciertos);
+       
     } else  {
-       alert('suma un error');
+    	$('#guardar1').unbind("click");
+        $('#guardar1').css('color', 'black');
+       
    }
 });
 
+/*----SEGUNDA PREGUNTA--------------------------------*/
+
+
+$('#guardar2').click(function() {
+
+   if ($('#vestir').val() === 'vestir'||$('#vestir').val() === 'ropa'||$('#vestir').val() === 'bestir') 
+     {
+     	aciertos++
+     	$('#guardar1').unbind("click");
+     	$('#guardar1').css('color', 'black');
+     	alert(aciertos);
+       
+    } else  {
+    	$('#guardar1').unbind("click");
+        $('#guardar1').css('color', 'black');
+       
+   }
+});
