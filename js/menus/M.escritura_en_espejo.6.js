09ce14c66800ed8d9ffc025fd456963600bd5038
javier.canchon <javier.canchon@hotmail.com>
2020-03-08 22:25:10 -0500

    se añade menu de la prueba 6

---
+++ js/menus/M.escritura_en_espejo.6.js
@@ -0,0 +1,102 @@
+var comision = 0;
+var aciertos = 0;
+var omision = 46;
+
+
+$("#boton2").css("display", "none");
+
+
+
+$("#L1").click(function() {
+    $('#L1').css('border-color', 'red');
+    aciertos++;
+    omision--;
+  
+
+    if (aciertos ==3) {
+
+    	$("#boton2").css("display", "block");
+    }
+
+});
+
+
+$("#L2").click(function() {
+    $('#L2').css('border-color', 'red');
+    aciertos++;
+    omision--;
+  
+
+    if (aciertos ==3) {
+
+    	$("#boton2").css("display", "block");
+    }
+
+});
+
+$("#L3").click(function() {
+    $('#L3').css('border-color', 'red');
+    aciertos++;
+    omision--;
+  
+
+    if (aciertos ==3) {
+
+    	$("#boton2").css("display", "block");
+    }
+
+});
+
+$("#L4").click(function() {
+    $('#L4').css('border-color', 'red');
+    aciertos++;
+    omision--;
+  
+
+    if (aciertos ==3) {
+
+    	$("#boton2").css("display", "block");
+    }
+
+});
+
+$("#T1").click(function() {
+    $('#T1').css('border-color', 'red');
+    aciertos++;
+    omision--;
+  
+
+    if (aciertos ==3) {
+
+    	$("#boton2").css("display", "block");
+    }
+
+});
+
+
+$("#T2").click(function() {
+    $('#T2').css('border-color', 'red');
+    aciertos++;
+    omision--;
+  
+
+    if (aciertos ==3) {
+
+    	$("#boton2").css("display", "block");
+    }
+
+});
+
+
+$("#T3").click(function() {
+    $('#T3').css('border-color', 'red');
+    aciertos++;
+    omision--;
+  
+
+    if (aciertos ==3) {
+
+    	$("#boton2").css("display", "block");
+    }
+
+});
