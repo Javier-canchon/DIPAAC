70eb69c7e3c579c2c391c4466e9266032edc0aad
javier.canchon <javier.canchon@hotmail.com>
2020-03-19 13:27:48 -0500

    se agregan nuevas preguntas a la prueba 9

--- js/prueba9.js
+++ js/prueba9.js
@@ -11,6 +11,16 @@ $("#llanta").css("display", "none");
 $("#plato").css("display", "none");
 $("#comida").css("display", "none");
 
+$("#oso").css("display", "none");
+$("#carro").css("display", "none");
+$("#amigo").css("display", "none");
+
+$("#come").css("display", "none");
+$("#rie").css("display", "none");
+$("#pinta").css("display", "none");
+
+
+
 
 $('#Conejo').click(function() {
 	$('#Conejo').css('color', 'black');
@@ -74,7 +84,7 @@ $('#Llanta').click(function() {
 
 $('#Comida').click(function() {
 	$('#Comida').css('color', 'black');
-	$("#espacio").css("display","none");
+	$("#espacio2").css("display","none");
          $("#comida").css("display","inline");
 
         $('#Llanta').unbind("click");
@@ -87,7 +97,7 @@ $('#Comida').click(function() {
 
 $('#Plato').click(function() {
 	$('#Plato').css('color', 'black');
-	$("#espacio").css("display","none");
+	$("#espacio2").css("display","none");
          $("#plato").css("display","inline");
 
      		$('#Llanta').unbind("click");
@@ -95,6 +105,152 @@ $('#Plato').click(function() {
          $('#Comida').unbind("click");
      	
 });
+
+
+
+//-------------------------tercera pregunta------------------------------------------
+
+
+
+
+
+$('#Oso').click(function() {
+	$('#Oso').css('color', 'black');
+
+	$("#espacio3").css("display","none");
+         $("#oso").css("display","inline");
+        
+         $('#Oso').unbind("click");
+         $('#Amigo').unbind("click");
+         $('#Carro').unbind("click");
+          aciertos ++;
+     	errores --;
+
+     	
+  
+});
+
+$('#Amigo').click(function() {
+	$('#Amigo').css('color', 'black');
+	$("#espacio3").css("display","none");
+         $("#amigo").css("display","inline");
+
+        $('#Oso').unbind("click");
+         $('#Amigo').unbind("click");
+         $('#Carro').unbind("click");
+     	
+});
+
+$('#Carro').click(function() {
+	$('#Carro').css('color', 'black');
+	$("#espacio3").css("display","none");
+         $("#carro").css("display","inline");
+
+     		$('#Oso').unbind("click");
+         $('#Amigo').unbind("click");
+         $('#Carro').unbind("click");
+     	
+});
+
+
+
+
+//-------------------------cuarta pregunta------------------------------------------
+
+
+
+
+
+$('#Pinta').click(function() {
+	$('#Pinta').css('color', 'black');
+
+	$("#espacio4").css("display","none");
+         $("#pinta").css("display","inline");
+        
+         $('#Pinta').unbind("click");
+         $('#Come').unbind("click");
+         $('#Rie').unbind("click");
+          aciertos ++;
+     	errores --;
+     	alert(aciertos);
+     	
+  
+});
+
+$('#Rie').click(function() {
+	$('#Rie').css('color', 'black');
+	$("#espacio4").css("display","none");
+         $("#rie").css("display","inline");
+
+        $('#Pinta').unbind("click");
+         $('#Come').unbind("click");
+         $('#Rie').unbind("click");
+});
+
+$('#Come').click(function() {
+	$('#Come').css('color', 'black');
+	$("#espacio4").css("display","none");
+         $("#come").css("display","inline");
+
+     		$('#Pinta').unbind("click");
+         $('#Come').unbind("click");
+         $('#Rie').unbind("click");
+});
+
+//-------------------------quinta pregunta------------------------------------------
+
+
+
+
+
+$('#Oso').click(function() {
+	$('#Oso').css('color', 'black');
+
+	$("#espacio3").css("display","none");
+         $("#oso").css("display","inline");
+        
+         $('#Oso').unbind("click");
+         $('#Amigo').unbind("click");
+         $('#Carro').unbind("click");
+          aciertos ++;
+     	errores --;
+
+     	
+  
+});
+
+$('#Amigo').click(function() {
+	$('#Amigo').css('color', 'black');
+	$("#espacio3").css("display","none");
+         $("#amigo").css("display","inline");
+
+        $('#Oso').unbind("click");
+         $('#Amigo').unbind("click");
+         $('#Carro').unbind("click");
+     	
+});
+
+$('#Carro').click(function() {
+	$('#Carro').css('color', 'black');
+	$("#espacio3").css("display","none");
+         $("#carro").css("display","inline");
+
+     		$('#Oso').unbind("click");
+         $('#Amigo').unbind("click");
+         $('#Carro').unbind("click");
+     	
+});
+
+
+
+
+
+
+
+
+
+
+
 
 
 
